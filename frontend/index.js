605292661ae1ed582c6974e0fc25beeb3a47c38d
EdgarMTdev <edgarmontiel961@gmail.com>
2022-07-12 23:38:24 -0500

    add: ui of example for chat

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,35 @@
-const socket  = io.connect('http://localhost:4000/')
+const socket = io.connect('http://localhost:4000/')
 const login = document.getElementById('login')
+const chat = document.getElementById('messages')
+const chatCont = document.getElementById('chat')
+const activeUsers = document.getElementById('actives')
 
 login.addEventListener('submit', (event) => {
     event.preventDefault()
     const { username } = event.target
-    console.log(username.value);
     socket.emit('active', username.value)
+    username.value = ''
 })
 
 socket.on('user connected', (users) => {
-    console.log(users)
+    activeUsers.innerHTML = ''
+    users.forEach(user => {
+        const li = document.createElement('li')
+        li.innerText = user
+        activeUsers.appendChild(li)
+    });
 })
+
+messages.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const { message } = event.target
+    socket.emit('send message', message.value)
+    message.value = ''
+})
+
+socket.on('res message', (message) => {
+    const li = document.createElement('li')
+    li.innerText = message
+    chatCont.appendChild(li)
+})
+
