741c8f75e3662e463f9d89878eca8ad274faba3d
EdgarMTdev <edgarmontiel961@gmail.com>
2022-07-12 23:37:43 -0500

    add: test chat with sockets

--- backend/src/services/chat.js
+++ backend/src/services/chat.js
@@ -8,14 +8,17 @@ class Chat {
             console.log('A user connected')
             socket.on('active', (data) => {
                 users.push(data)
-                console.log(data)
-                console.log(users)
                 io.emit('user connected', users)
             })
 
             socket.on('disconnect', () => {
                 users.pop()
                 io.emit('user disconnected', users)
+            })
+
+            socket.on('send message', (message) => {
+                messages.push(message)
+                io.emit('res message', message)
             })
         })
     }
