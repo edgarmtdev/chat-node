fe3b5cc7b75469764461062f125105e024ca0a87
EdgarMTdev <edgarmontiel961@gmail.com>
2022-07-17 23:37:24 -0500

    add: private messages with id of sockets

--- backend/src/services/chat.js
+++ backend/src/services/chat.js
@@ -1,17 +1,17 @@
 import cookie from 'cookie'
 import Auth from './auth'
 let users = []
-const messages = []
+let chats = []
+let messages = []
 
 class Chat {
     constructor(io) {
         this.io = io
         this.io.on('connection', (socket) => {
-            console.log('A user connected')
             socket.on('active', (data) => {
-                const cookies = cookie.parse(socket.handshake.headers.cookie)
-                const userData = Auth.validateToken(cookies.token)
-                console.log(userData)
+                // const cookies = cookie.parse(socket.handshake.headers.cookie)
+                // const userData = Auth.validateToken(cookies.token)
+                // console.log(userData)
                 socket.idUser = data
                 users.push({
                     idUser: data,
@@ -29,9 +29,30 @@ class Chat {
             })
 
             socket.on('send message', (idSocket, message) => {
-                socket.to(idSocket)
-                messages.push(message)
-                io.emit('res message', message)
+                const idChat = socket.idUser + idSocket
+                const newChat = {
+                    id: idChat,
+                    sender: socket.idUser,
+                    received: idSocket,
+                    messages: [...messages, message]
+                }
+
+                if (chats.length === 0) {
+                    chats.push(newChat)
+                    console.log(chats);
+                    socket.to(idSocket).emit('private send message', newChat)
+                    io.to(socket.idUser).emit('message sended', newChat)
+                } else {
+                    chats.forEach(chat => {
+                        if (chat.id === idChat) {
+                            chat.messages.push(message)
+                            socket.to(idSocket).emit('private send message', chat)
+                            io.to(socket.idUser).emit('message sended', chat)
+                        } else {
+                            chats.push(newChat)
+                        }
+                    })
+                }
             })
         })
     }
