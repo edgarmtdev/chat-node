ac617fd1f8b8cb0852413bf6e650fbf461b42584
EdgarMTdev <edgarmontiel961@gmail.com>
2022-08-01 09:25:47 -0500

    refactor: router of de pages

--- frontend/js/components/login.js
+++ frontend/js/components/login.js
@@ -7,6 +7,13 @@ export const Login = () => {
     const login = document.createElement('form')
     login.id = 'login'
 
+    const title = createElement('h1', {
+        attrs: [
+            {name: 'class', value: 'login-title'}
+        ],
+        content: 'Login'
+    })
+
     const email = createElement('input', {
         attrs: [
             { name: 'type', value: 'text' },
@@ -27,21 +34,28 @@ export const Login = () => {
         ],
         content: 'SignIn'
     })
-
+    login.appendChild(title)
     login.appendChild(email)
     login.appendChild(password)
     login.appendChild(button)
     return login
 }
 
- 
+
 
 export function auth() {
     const authLogin = document.getElementById('login')
     authLogin.addEventListener('submit', (event) => {
         event.preventDefault()
-        const { email: { value: email }, password: { value: password } } = event.target
+        const {
+            email: {
+                value: email
+            },
+            password: {
+                value: password
+            }
+        } = event.target
 
-        login({email, password})
+        login({ email, password })
     })
 }
--- frontend/js/router/index.js
+++ frontend/js/router/index.js
@@ -7,18 +7,22 @@ import { auth, Login } from "../components/login.js"
 import Users from "../components/users.js"
 
 const root = document.getElementById('root')
+const usersActives = document.getElementById('active-users')
 
 const router = async (path) => {
     const [socket, user] = await validate()
     root.innerHTML = ''
     switch (path) {
         case '#/':
+            usersActives.style.display = 'block'
             root.appendChild(Chat())
             if (user.logged) return addEvents(socket)
         case '#/login':
+            usersActives.style.display = 'none'
             root.appendChild(Login())
             return auth()
         case '#/people':
+            usersActives.style.display = 'block'
             const users = await getUsers()
             if (users) {
                 return root.appendChild(Users(users))
@@ -26,7 +30,8 @@ const router = async (path) => {
         case `#/t/`: 
             return
         case '':
-            return root.innerHTML = `<div>Hola</>`
+            usersActives.style.display = 'none'
+            return root.innerText = `Inicio`
     }
 }
 
